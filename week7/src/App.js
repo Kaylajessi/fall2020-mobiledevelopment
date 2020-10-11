67c38f77654890bda42771526067ada548b7427e
Kaylajessi <robertsjkayla@knights.ucf.edu>
2020-10-10 20:22:47 -0400

    week 7

---
+++ week7/src/App.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import React, {Component} from 'react';
+import './App.css';
+
+function App() {
+
+  let [example, changeExample] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+       <p>Count: {example}</p>
+       <button onClick={() => {changeExample(example + 2)}}>Increase!</button>
+      </header>
+    </div>
+  );
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0
+    };
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Count: {this.state.counter}</p>
+          <button onClick={() => {this.setState({counter: this.state.counter + 1});
+          }}>Increase!</button>
+          </header>
+      </div>
+    )
+  }
+}
+
+export default App;
