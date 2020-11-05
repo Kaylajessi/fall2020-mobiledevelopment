ff7eca8ab16d0c0db0429dbc707c9355d8e4b9e0
Kaylajessi <robertsjkayla@knights.ucf.edu>
2020-11-05 13:02:21 -0500

    week11

---
+++ week11/App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Card, Button } from 'react-native-elements';
+
+export default function App() {
+  return (
+    <View>
+      <Card>
+        <Card.Title>Kayla</Card.Title>
+        <Text>Sushi</Text>
+        <Card.Divider/>
+        <Text>Pizza</Text>
+        <Button title="Submit"></Button>
+      </Card>
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
