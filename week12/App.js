397d6f87270f4a8164d8deddd2b94b21629f04e2
Kaylajessi <robertsjkayla@knights.ucf.edu>
2020-11-14 10:38:08 -0500

    week12

--- week12/App.js
+++ week12/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Card, Button } from 'react-native-elements';
 
-
 export default function App() {
+  const [pressed, setPressed] = useState()
+  const shouldShow = pressed === undefined 
   return (
-    <View style={styles.container}>
-      <Card>
-        <Card.Title style={styles.header}>Vote!</Card.Title>
-        <Text>Please vote for your preferred choice of food!</Text>
-        <Card.Divider/>
-        <Button title="Pizza" onPress={ () => {setPressed(!pressed)} } />
-        <Card.Divider/>
-        <Button title="Burger" onPress={ () => {setPressed(!pressed)} } />
-        <Card.Divider/>
-        <Button title="Wings" onPress={ () => {setPressed(!pressed)} } />
-      </Card>
-    </View>
+       <View style={styles.container}>
+         {shouldShow &&
+          <Card>
+              <Card.Title style={styles.header}>Vote!</Card.Title>
+              <Text>Please vote for your preferred choice of food</Text>
+              <Card.Divider/>
+              <Button title="Pizza" onPress={ () => {setPressed("Pizza")}} />
+              <Card.Divider/>
+              <Button title="Burger" onPress={ () => {setPressed("Burger")}} />
+              <Card.Divider/>
+              <Button title="Wings" onPress={ () => {setPressed("Wings")}} />
+          </Card>
+         }
+          <Card>
+            {pressed}
+          </Card> 
+        </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
