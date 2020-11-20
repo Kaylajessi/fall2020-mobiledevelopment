f327ee40435d4e26e0b5aecf63b7cf3714612385
Kaylajessi <robertsjkayla@knights.ucf.edu>
2020-11-19 20:23:32 -0500

    week13drawer

--- week13drawer/App.js
+++ week13drawer/App.js
@@ -1,38 +1,54 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native-elements';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
-export default function App() {
+function HomeScreen(props) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button style="auto" />
+      <Text>Activities!</Text>
+      <Button 
+        title="Fencing"
+        onPress={() => props.navigation.navigate("Fencing")}/>
+      <Button 
+        title="Biking"
+        onPress={() => props.navigation.navigate("Biking")}/>
     </View>
   );
 }
 
-function FilesScreen(props) {
+
+function FencingScreen() {
+  const [track, setTrack] = useState(0)
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+      <Text>Fencing count: {track}</Text>
       <Button
-        title="Settings"
-        onPress={() => props.navigation.navigate('Settings')}
+        title="Increase"
+        onPress={() => setTrack(track + 1)}
+      />
+      <Button
+        title="Reset"
+        onPress={() => setTrack(0)}
       />
     </View>
   );
 }
 
-function SettingsScreen(props) {
+function BikingScreen() {
+  const [track, setTrack] = useState(0)
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+      <Text>Biking count: {track}</Text>
       <Button
-        title="Back to Home"
-        onPress={() => props.navigation.navigate('Home')}
+        title="Increase"
+        onPress={() => setTrack(track + 1) }
+      />
+      <Button
+        title="Reset"
+        onPress={() => setTrack(0)}
       />
     </View>
   );
@@ -40,14 +56,21 @@ function SettingsScreen(props) {
 
 const Drawer = createDrawerNavigator();
 
-function Home() {
+function Home(screen1, screen2) {
   return (
     <NavigationContainer>
       <Drawer.Navigator>
-        <Drawer.Screen name="Files" component={FilesScreen} />
-        <Drawer.Screen name="Settings" component={SettingsScreen} />
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Fencing" component={screen1} />
+        <Drawer.Screen name="Biking" component={screen2} />
       </Drawer.Navigator>
     </NavigationContainer>
+  );
+}
+
+function App(){
+  return Home(
+    FencingScreen, BikingScreen
   );
 }
 
